9fb5e6e47a74dad0df5ac6a551dfa0879bd3b05f
sriram1990usa <60535385+sriram1990usa@users.noreply.github.com>
2024-12-19 22:16:34 +0530

    Update settings.py

--- deploy_on_pythonanywhere/settings.py
+++ deploy_on_pythonanywhere/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g7mqbv+(u@1di1vdgo+x7xbwnc77^=j&^s@r*af)hbx7ec8v)p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
